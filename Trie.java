80e0e6a7871b2db6f88c7094a0e19af1c7827daa
Brian Schott <bschott@cgps.org>
2024-02-28 11:14:10 -0500

    Add files via upload

---
+++ Trie.java
@@ -0,0 +1,21 @@
+// Java implementation of search and insert operations
+// on Trie
+//Stolen and modified from Baeldung
+
+public class Trie {
+
+
+    //base of the trie
+    public static TrieNode root;
+
+
+
+
+    // If not present, inserts key into trie
+    // If the key is prefix of trie node,
+    // just marks leaf node
+    public static void insert(String key) {
+
+    }
+
+}
